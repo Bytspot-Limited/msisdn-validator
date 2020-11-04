69f8f6a2af404e5be3269c16831c45e95e4c5825
Eli Muraya <emuraya@one-point-seven.com>
2020-11-04 10:05:45 +0300

    Added : * docker-compose.yml for container creation * start_up.sh for wrapping up the startup process of the app

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:14-alpine
+FROM adoptopenjdk/openjdk11:jdk-11.0.6_10-alpine-slim
 COPY target/msisdn-validator-*.jar msisdn-validator.jar
 EXPOSE 8080
 CMD ["java", "-Dcom.sun.management.jmxremote", "-Xmx128m", "-jar", "msisdn-validator.jar"]
---
+++ start_up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "======================================================================="
+echo ""
+echo "Building project with tests ..."
+mvn clean install
+echo "Build successful"
+echo "======================================================================="
+echo ""
+echo "Building docker image ..."
+build=$(docker build -t jambopay/msisdn-validator:1.0-SNAPSHOT .)
+echo "Docker image built: $build"
+echo "======================================================================="
+echo ""
+
+echo "Starting new container instance ..."
+docker-compose down --remove-orphans && docker-compose up -d
+echo "Container started successfully."
+echo "======================================================================="
+echo ""
